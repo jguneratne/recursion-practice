6ddd4cdbd7c8fe75c176585e05752d8e0073c238
jguneratne <59374962+jguneratne@users.noreply.github.com>
2025-03-24 08:19:42 -0400

    Solved Q8

--- Q8.js
+++ Q8.js
@@ -3,14 +3,41 @@
 // Question 8:
 // Write a function that sums squares of numbers in list that may contain more lists
 
-let l = [1, 2, 3];
-console.log(SumSquares(l)); // 1 + 4 + 9 = 14
+const arrOne = [1, 2, 3];
 
-l = [[1, 2], 3];
-console.log(SumSquares(l)); // 1 + 4 + 9 = 14
+const arrTwo = [[1, 2], 3];
 
-l = [[[[[[[[[1]]]]]]]]];
-console.log(SumSquares(l)); // 1 = 1
+const arrThree = [[[[[[[[[1]]]]]]]]];
 
-l = [10, [[10], 10], [10]];
-console.log(SumSquares(l)); // 100 + 100 + 100 + 100 = 400
+const arrFour = [10, [[10], 10], [10]];
+
+function sumSquares(arr) {
+  let total = 0;
+
+  // Base Case
+  if (!Array.isArray(arr) && Number.isInteger(arr)) {
+    // return the square of the numbers
+    return Math.pow(arr, 2);
+  } else if (!Array.isArray(arr) && !Number.isInteger(arr)) {
+    return 0;
+  } else {
+    // Recursive Case
+    for (let i = 0; i < arr.length; i++) {
+      total += sumSquares(arr[i]);
+    }
+  }
+
+  return total;
+}
+
+// let sumSquaresOne = sumSquares(arrOne);
+// console.log(sumSquaresOne); // 1 + 4 + 9 = 14
+
+// let sumSquaresTwo = sumSquares(arrTwo);
+// console.log(sumSquaresTwo); // 1 + 4 + 9 = 14
+
+// let sumSquaresThree = sumSquares(arrThree);
+// console.log(sumSquaresThree); // 1 = 1
+
+let sumSquaresFour = sumSquares(arrFour);
+console.log(sumSquaresFour); // 100 + 100 + 100 + 100 = 400
